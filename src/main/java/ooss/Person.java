17e02ce80d4276ac8f177e74d4fc9b40ef423d4a
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-16 17:26:45 +0800

    feat: added functionality to return message with name and age when introduce given a person

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,5 +1,17 @@
 package ooss;
 
 public class Person {
+    private final int id;
+    private final String name;
+    private final int age;
 
+    public Person(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
