b389c929b8245431468a92ba104329a58e5753dc
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-16 19:35:20 +0800

    feat: added functionality for teacher to introduce with their assigned classes if any

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -2,17 +2,25 @@ package ooss;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class Teacher extends Person{
+public class Teacher extends Person {
     private final List<Klass> classes;
+
     public Teacher(int id, String name, int age) {
         super(id, name, age);
         classes = new ArrayList<>();
     }
 
     @Override
-    public String introduce(){
-        return super.introduce() + " I am a teacher.";
+    public String introduce() {
+        return super.introduce() + " I am a teacher."
+                + (this.classes.size() == 0 ? "" : " I teach Class " +
+                (this.classes.size() == 1 ? this.classes.get(0).number :
+                        this.classes.stream()
+                                .map(klass -> String.valueOf(klass.number))
+                                .collect(Collectors.joining(", ")) + "."));
     }
 
     public void assignTo(Klass klass) {
