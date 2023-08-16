e92ecf8c18ea4207a9ce5326f09b0b3b14398eeb
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-16 19:17:45 +0800

    feat: added functionality to assign teacher and check if teacher belongs to a klass

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,17 +1,25 @@
 package ooss;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Teacher extends Person{
+    private final List<Klass> classes;
     public Teacher(int id, String name, int age) {
         super(id, name, age);
+        classes = new ArrayList<>();
     }
 
     @Override
     public String introduce(){
         return super.introduce() + " I am a teacher.";
+    }
+
+    public void assignTo(Klass klass) {
+        this.classes.add(klass);
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return this.classes.contains(klass);
     }
 }
