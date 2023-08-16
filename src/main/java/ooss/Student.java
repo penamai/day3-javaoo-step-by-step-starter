7e27f11ac70d6e5f236a5094fd5acfc05467b0c2
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-16 18:20:37 +0800

    feat: added functionality to return message with name, age, and student or teacher

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,5 +1,12 @@
 package ooss;
 
-public class Student extends Person {
+public class Student extends Person{
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
 
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a student.";
+    }
 }
--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -5,5 +5,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Teacher extends Person {
+public class Teacher extends Person{
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a teacher.";
+    }
 }
